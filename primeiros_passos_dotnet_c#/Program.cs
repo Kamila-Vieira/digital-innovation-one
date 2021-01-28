eba9d018263153da1ec5321b7457a892a961b451
Kamila Almeida <vkamila54@outlook.com>
2021-01-27 22:30:21 -0300

    Adicionando curso .NET+C#

---
+++ primeiros_passos_dotnet_c#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using primeiros_passos_dotnet_c_.Exemplos;
+
+namespace primeiros_passos_dotnet_c_
+{
+  class Program
+  {
+    static void Main()
+    {
+      /* AULA 1 */
+      int numeroDeVezes = 5;
+      for (int i = 1; i <= numeroDeVezes; i++)
+      {
+        Console.WriteLine($"Bem-vindo ao curso de .NET {i}");
+      }
+      /* AULA 2 */
+      var pilha = new Pilha();
+      pilha.Empilha(1);
+      pilha.Empilha(10);
+      pilha.Empilha(100);
+      Console.WriteLine(pilha.Desempilha());
+      Console.WriteLine(pilha.Desempilha());
+      Console.WriteLine(pilha.Desempilha());
+      //Console.WriteLine(pilha.Desempilha());
+    }
+  }
+}
