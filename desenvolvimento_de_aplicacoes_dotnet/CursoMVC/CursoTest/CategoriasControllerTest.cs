c5ae5aa1c9bd64118b260edb58c3b1cf30724eb1
kamila.almeida <vkamila54@outlook.com>
2021-01-29 20:27:01 -0300

    Update API class

---
+++ desenvolvimento_de_aplicacoes_dotnet/CursoMVC/CursoTest/CategoriasControllerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+
+namespace CursoTest
+{
+    public class CategoriasControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+        public CategoriasControllerTest()
+        {
+
+        }
+    }
+}
---
+++ desenvolvimento_de_aplicacoes_dotnet/CursoMVC/CursoTeste/CategoriasControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using CursoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoTeste
+{
+   
+    public class CategoriasControllerTest
+    {
+        private readonly Mock<DbSet<Categoria>> _mockSet;
+        private readonly Mock<Context> _mockContext;
+        private readonly Categoria _categoria;
+        public CategoriasControllerTest()
+        {
+
+        }
+    }
+}
