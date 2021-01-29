083a1a3a0ad8a401cefca153e5f7cd120e0bfa91
kamila.almeida <vkamila54@outlook.com>
2021-01-29 17:47:36 -0300

    Add final class 1 application .NET

--- desenvolvimento_de_aplicacoes_dotnet/CursoMVC/CursoMVC/Models/Context.cs
+++ desenvolvimento_de_aplicacoes_dotnet/CursoMVC/CursoMVC/Models/Context.cs
@@ -10,6 +10,7 @@ namespace CursoMVC.Models
     public class Context : DbContext
     {
         public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
