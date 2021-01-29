6aeb0516b7f9aa6e5d29e14e78b075db4b89f572
kamila.almeida <vkamila54@outlook.com>
2021-01-29 10:02:44 -0300

    Add application .NET class

---
+++ desenvolvimento_de_aplicacoes_dotnet/CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Cursomvc;Integrated Security=True");
+        }
+    }
+}
